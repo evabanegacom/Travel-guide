1030c89b53c1780e48a39b14ed29e818e9624791
Precious Udegbue <udegbue69@gmail.com>
2021-12-26 20:36:10 +0100

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+export const getPlacesData = async (sw, ne) => {
+    try {
+        // request
+        const { data: { data } } = await axios.get(URL, {
+        
+                params: {
+                  bl_latitude: sw.lat,
+                  tr_latitude: ne.lat,
+                  bl_longitude: sw.lng,
+                  tr_longitude: ne.lng,
+                },
+                headers: {
+                  'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+                  'x-rapidapi-key': '38b9d1c426mshc76e85e64d7ae07p12c975jsn4cc4737ae84b'
+                }
+            }
+            )
+
+        return data
+    } catch(error) {
+        console.log(error)
+    }
+}
