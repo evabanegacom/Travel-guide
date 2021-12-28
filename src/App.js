cfcd25cc9c21ed00f7045f6cf01ab034909cec30
Precious Udegbue <udegbue69@gmail.com>
2021-12-28 10:57:07 +0100

    added filtering and sorting

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ const App = () => {
   const [ places, setPlaces ] = useState([]);
   const [ childClicked, setChildClicked ] = useState(null)
   const [ isLoading, setIsLoading ] = useState(false);
-
+  const [ type, setType ] = useState('restaurants')
+  const [ rating, setRating ] = useState('')
+  const [ filteredPlaces, setFilteredPlaces ] = useState([]);
   const [ coordinates, setCoordinates ] = useState({});
   const [ bounds, setBounds ] = useState(null);
 
@@ -20,30 +22,42 @@ const App = () => {
     })
   }, [])
 
+  useEffect(() => {
+    const filteredPlaces = places?.filter(place => place.rating > rating)
+
+    setFilteredPlaces(filteredPlaces)
+  }, [rating])
+
   useEffect(() =>{
     setIsLoading(true);
-    getPlacesData(bounds?.sw, bounds?.ne)
+    getPlacesData(type, bounds?.sw, bounds?.ne)
     .then((data) => {
       setPlaces(data)
+      setFilteredPlaces([])
       setIsLoading(false);
     })
-  }, [coordinates, bounds])
+  }, [type, coordinates, bounds])
   return (
     <>
       <CssBaseline />
-      <Header />
+      <Header setCoordinates={setCoordinates} />
       <Grid container spacing={3} style={{ width: '100%'}}>
         <Grid item xs={12} md={4}>
-          <List places={places}
-          childClicked={childClicked}
+          <List 
+            places={filteredPlaces?.length ? filteredPlaces : places}
+            childClicked={childClicked}
           isLoading={isLoading}
+          type={type}
+          setType={setType}
+          rating={rating}
+          setRating={setRating}
           />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
-            places={places}
+            places={filteredPlaces?.length ? filteredPlaces : places}
             setChildClicked={setChildClicked} 
           />
         </Grid>
